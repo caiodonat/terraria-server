9add2b522ff539163bd424eb62196cabfdbaa81e
caiodonat <cdonat.dev@gmail.com>
2026-02-16 12:15:55 -0300

    fix(service): screen parameters and use bash

--- server/start-server.sh
+++ server/start-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/terraria-server/server/official-server/TerrariaServer.bin.x86_64 -config /terraria-server/server/serverconfig.txt >> /terraria-server/server/official-server.log 2>&1 &
+/terraria-server/server/official-server/TerrariaServer.bin.x86_64 -config /terraria-server/server/serverconfig.txt >> /terraria-server/server/official-server.log
